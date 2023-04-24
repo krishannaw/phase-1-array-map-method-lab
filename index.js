97e0d2cc400ec445ea6510e8d5fef7ba4e2e1cae
Krishanna Williams <krishannaw@gmail.com>
2023-04-23 21:58:18 -0500

    hard

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "What Does the this Keyword Mean?",
+"What is the Constructor OO Pattern?",
+"Implementing Blockchain Web API",
+"The Test Driven Development Workflow",
+"What is NaN and How Can We Check for It",
+"What is the Difference Between stopPropagation and preventDefault?",
+"Immutable State and Pure Functions",
+"What is the Difference Between == and ===?",
+"What is the Difference Between Event Capturing and Bubbling?",
+"What is JSONP?"
 ];
-
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function(tutorial) {
+    const words = tutorial.split(' ');
+    const titleCasedWords = words.map(function(word) {
+      return word[0].toUpperCase() + word.slice(1);
+    });
+    return titleCasedWords.join(' ');
+  });
 }
+
